c4c923e7c28e9a15ac1c10e9378b16d2d3c49bcf
Nstprog03 <86487276+Nstprog03@users.noreply.github.com>
2022-03-10 11:38:11 +0530

    admin-login created

--- Cybercom/core/Project/Model/Vendor/Address.php
+++
@@ -1,9 +0,0 @@
-<?php Ccc::loadClass('Model_Core_Row');
-class Model_Vendor_Address extends Model_Core_Row
-{
-	public function __construct()
-	{
-		$this->setResourceClassName('Vendor_Address_Resource');
-		parent::__construct();
-	}
-}
